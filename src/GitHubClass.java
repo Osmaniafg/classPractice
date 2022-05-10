40fcdd501d3cb81bb51c2970892f3d903f59be4c
Waheed Ahmad Osmani <waheed.osmani@yahoo.com>
2022-05-09 22:05:45 -0400

    Class is updated

--- src/GitHubClass.java
+++ src/GitHubClass.java
@@ -2,7 +2,13 @@
 public class GitHubClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int a=4;
+		int b=5;
+		int c=a+b;
+		
+		System.out.println(c);
+
 
 	}
 
